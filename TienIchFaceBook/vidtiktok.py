9fc234068ab6bd543df84263547feefdc8fee8bd
ANH CODE <nhatanh171008@gmail.com>
2025-03-29 01:55:13 +0700

    Add files via upload

---
+++ TienIchFaceBook/vidtiktok.py
@@ -0,0 +1,143 @@
+import os
+import requests
+from urllib.parse import urlparse
+import time
+from time import sleep
+from datetime import date, datetime
+import pywifi
+from pystyle import *
+import pystyle
+lam = "\033[1;36m"
+do = "\033[1;31m"
+luc = "\033[1;32m"
+vang = "\033[1;33m"
+xduong = "\033[1;34m"
+hong = "\033[1;35m"
+trang = "\033[1;37m"
+whiteb="\033[1;37m"
+red="\033[0;31m"
+redb="\033[1;31m"
+end='\033[0m'
+data_machine = []
+today = date.today()
+now = datetime.now()
+thu = now.strftime("%A")
+ngay_hom_nay = now.strftime("%d")
+thang_nay = now.strftime("%m")
+nam_ = now.strftime("%Y")
+def check_internet_connection():
+    try:
+        response = requests.get("http://www.google.com", timeout=5)
+        return True
+    except requests.ConnectionError:
+        return False
+
+# Kiểm tra kết nối internet
+if check_internet_connection():
+    print(f"{luc}Vui Lòng Chờ!!!")
+    sleep(0.1)
+else:
+    print(f"{do}Vui Lòng Kiểm Tra Kết NốI!!!")
+    System.exit()
+def get_location_by_ip():
+    try:
+        response = requests.get("https://ipinfo.io")
+        data = response.json()
+
+        city = data.get("city")
+        region = data.get("region")
+        country = data.get("country")
+        loc = data.get("loc").split(",")
+        latitude, longitude = loc if len(loc) == 2 else (None, None)
+
+        return city, region, country, latitude, longitude
+    except Exception as e:
+        print(f"Lỗi: {e}")
+        return None, None, None, None, None
+city, region, country, latitude, longitude = get_location_by_ip()
+def get_weather():
+    try:
+        # Lấy thông tin vị trí từ dịch vụ ipinfo.io
+        response = requests.get("https://ipinfo.io")
+        data = response.json()
+        location = data.get("loc").split(",")
+        latitude, longitude = location
+        # Lấy thông tin thời tiết từ trang web công cộng
+        base_url = f"https://wttr.in/{latitude},{longitude}?format=%t"
+        response = requests.get(base_url)
+        weather_description = response.text.strip()
+        return weather_description
+    except Exception as e:
+        print(f"Lỗi: {e}")
+        return None
+weather_description = get_weather()
+System.Clear()
+def extract_video_id(video_url):
+    # Trích xuất ID video từ URL
+    parsed_url = urlparse(video_url)
+    path_parts = parsed_url.path.split("/")
+    video_id = path_parts[-1]
+
+    return video_id
+
+def download_tiktok_video(video_url):
+    # Trích xuất ID video
+    video_id = extract_video_id(video_url)
+
+    # Tạo đường dẫn thư mục lưu trữ
+    download_dir = "video_tiktok_download_by_hoàng_nam"
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
+
+    # Tải video từ TikTok
+    video_url = f"https://www.tiktok.com/@tiktok/video/{video_id}"
+    response = requests.get(video_url)
+
+    # Lưu video vào thư mục
+    if response.status_code == 200:
+        video_file = os.path.join(download_dir, f"{video_id}.mp4")
+        with open(video_file, "wb") as file:
+            file.write(response.content)
+        print(f"""
+\033[1;34m╔═══════════╗
+\033[1;36m║▇◤▔▔▔▔▔▔▔◥▇║
+\033[1;36m║▇▏◥▇◣┊◢▇◤▕▇║
+\033[1;36m║▇▏▃▆▅▎▅▆▃▕▇║
+\033[1;36m║▇▏╱▔▕▎▔▔╲▕▇║
+\033[1;36m║▇◣◣▃▅▎▅▃◢◢▇║
+\033[1;36m║▇▇◣◥▅▅▅◤◢▇▇║
+\033[1;36m║▇▇▇◣╲▇╱◢▇▇▇║
+\033[1;36m║▇▇▇▇◣▇◢▇▇▇▇║
+\033[1;34m╚═══════════╝
+\033[1;97mTool By: \033[1;32mAnhhCode            \033[1;97mPhiên Bản: \033[1;32m4.0     
+\033[97m════════════════════════════════════════════════  
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Youtube\033[1;31m  : \033[1;97m☞ \033[1;36mAnhCode\033[1;31m♔ \033[1;97m☜
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Facebook\033[1;31m : \033[1;97mhttps://www.facebook.com/profile.php?id=61570408533569
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Telegram\033[1;31m : \033[1;97m☞\033[1;32mhttps://t.me/shareanhcode🔫\033[1;97m☜
+\033[97m════════════════════════════════════════════════
+""")
+        print(f"{luc}\033[1;97m[\033[1;91m❣\033[1;97m] \033[1;36m✈  \033[1;32mVideo đã được tải xuống thành công.")
+    else:
+        print(f"{do}\033[1;97m[\033[1;91m❣\033[1;97m] \033[1;36m✈  \033[1;32mKhông thể tải video.")
+
+# Nhập liên kết video TikTok
+print(f"""
+\033[1;34m╔═══════════╗
+\033[1;36m║▇◤▔▔▔▔▔▔▔◥▇║
+\033[1;36m║▇▏◥▇◣┊◢▇◤▕▇║
+\033[1;36m║▇▏▃▆▅▎▅▆▃▕▇║
+\033[1;36m║▇▏╱▔▕▎▔▔╲▕▇║
+\033[1;36m║▇◣◣▃▅▎▅▃◢◢▇║
+\033[1;36m║▇▇◣◥▅▅▅◤◢▇▇║
+\033[1;36m║▇▇▇◣╲▇╱◢▇▇▇║
+\033[1;36m║▇▇▇▇◣▇◢▇▇▇▇║
+\033[1;34m╚═══════════╝
+\033[1;97mTool By: \033[1;32mAnhhCode            \033[1;97mPhiên Bản: \033[1;32m4.0     
+\033[97m════════════════════════════════════════════════  
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Youtube\033[1;31m  : \033[1;97m☞ \033[1;36mAnhCode\033[1;31m♔ \033[1;97m☜
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Facebook\033[1;31m : \033[1;97mhttps://www.facebook.com/profile.php?id=61570408533569
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Telegram\033[1;31m : \033[1;97m☞\033[1;32mhttps://t.me/shareanhcode🔫\033[1;97m☜
+\033[97m════════════════════════════════════════════════
+""")
+video_link = input(f"{trang}\033[1;97m[\033[1;91m❣\033[1;97m] \033[1;36m✈  \033[1;32mNhập liên kết video TikTok: {vang}")
+download_tiktok_video(video_link)
